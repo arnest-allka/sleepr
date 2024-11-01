e77bf2f0c63b545ffdaa6eac3726ff1352a3192a
Arnest Allka <arisallkas@gmail.com>
2024-11-01 18:00:29 +0200

    Passport

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,5 +1,7 @@
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import {
   ReservationDocument,
   ReservationSchema,
@@ -10,6 +12,13 @@ import { ReservationsService } from './reservations.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
     DatabaseModule,
     DatabaseModule.forFeature([
       { name: ReservationDocument.name, schema: ReservationSchema },
