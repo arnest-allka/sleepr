67cad8bff6a2d6ef45e3be21b14ada4cd0530af4
Arnest Allka <arisallkas@gmail.com>
2024-11-04 13:19:01 +0200

    Common Auth Guard

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -1,6 +1,6 @@
+import { CurrentUser } from '@app/common';
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
-import { CurrentUser } from '../current-user.decorator';
-import { JwtAuthGuard } from '../guards/jwt-auth-guard';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserDocument } from './models/user.schema';
 import { UsersService } from './users.service';
